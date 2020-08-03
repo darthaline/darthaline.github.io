40b532de44c1e74119513e3d860ea295a94266ce
darthaline <alinartsibasova@gmail.com>
2020-08-03 19:18:08 +0200

    added tdprarepair rumble analysis

---
+++ TDPRarepairRumble2020/READMERR.md
@@ -0,0 +1,3 @@
+R code to plot the results of TDP Rarepair Rumble 2020
+
+Event info: https://tdp-shipping-dock.tumblr.com/rarepair-rumble
---
+++ TDPRarepairRumble2020/RarepairRumbleResponses3.R
@@ -0,0 +1,56 @@
+#install.packages("rstudioapi")
+library("rstudioapi")
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+data <- read.csv2 ('Rarepair Rumble Round 3.csv', header = TRUE, sep=',')
+data <- sapply(data, as.character)
+data[,'Where.did.you.find.the.link.to.this.round.'][data[,'Where.did.you.find.the.link.to.this.round.'] == '']<- "No Source Provided"
+data[data == "I don't want to vote for either of these ships"] <- "_choose not to vote"
+data[data == ""] <- "_choose not to vote"
+data[data == "Didn't see it on reddit so searched for it on Tumblr"] <- "Tumblr"
+data <- data.frame(data)
+
+titles <- colnames(data)
+titles[titles == "Where.did.you.find.the.link.to.this.round."] <- "source"
+titles <- sub("X.", "", titles)
+titles <- sub("[[:digit:]]", "", titles)
+titles <- sub("[[:digit:]]", "", titles)
+titles <- sub(".vs.", " vs ", titles)
+titles <- sub("[[:punct:]]", "", titles)
+titles <- sub("[[:punct:]]", "", titles)
+
+colnames(data)<- titles
+
+#install.packages("lubridate")
+library("lubridate")
+data$Timestamp <- parse_date_time2(as.character(data$Timestamp), orders = "%d/%m/%Y %H:%M:%S")
+
+#install.packages("ggplot2")
+library("ggplot2")
+#install.packages("cowplot")
+library("cowplot")
+#install.packages("RColorBrewer")
+library("RColorBrewer")
+custompalette <- brewer.pal(n=7, name = 'Dark2')
+display.brewer.pal(name = 'Dark2', n=7)
+
+pdf(file='RRResults_3_bar_total.pdf', paper ='USr', width=11.69, height=8.27)
+
+for (i in 2:17) {
+  ploti<- ggplot(data, aes(x=data[,i])) + 
+    geom_bar(aes(col=source, fill=source), alpha = 0.5)+
+    scale_colour_manual(values = custompalette)+
+    scale_fill_manual(values = custompalette)
+  
+  print(ploti +
+          theme_half_open() +
+          background_grid() +
+          theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),
+                               axis.title.y=element_blank(),
+                               axis.title.x=element_blank()))
+}
+
+dev.off()
+
+
+dev.off()
