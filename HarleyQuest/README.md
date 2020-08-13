8d5ac931013669d97019fc12940910d5a6a91552
darthaline <alinartsibasova@gmail.com>
2020-08-13 12:15:44 +0200

    added HarleyQuest

---
+++ HarleyQuest/README.md
@@ -0,0 +1,12 @@
+Template for HarleyQuest
+
+currently cookieClicker tutorial
+
+Features to be added
+
+Lootables for Salvage
+
+Decide resource flow
+
+Location based loot tables?
+
---
+++ HarleyQuest/js/Buildings.js
@@ -0,0 +1,131 @@
+
+//Buildings List//
+
+//Communications// - maybe special tab, shows plot etc - is messed up
+/*
+Satellite dish - unique
+Telescope? - provides notice of meteorites/spaceship
+Signal fire? - maybe unlocks carapacians
+*/
+
+//Food//
+/*
+Salvage cans
+Greenhouse - unique?
+Farms, dirt
+Farms, volcanic dirt
+Farms, Hydroponic
+Vertical farming?
+Orchard (would need some sort of timewarp)
+Fishing, manual
+Fishing, nets
+Hunting?
+
+*/
+
+//Misc// - mostly unlocks/unique
+/*
+Ruins access
+Volcano access
+North island access
+Greenhouse? - experimental food
+Workshop - improvise things
+Lab - research things
+Docks - for building ship
+*/
+
+//Metal//
+/*
+Salvage Manually
+Salvage sea wrecks
+Mining Volcano
+Mining rocks
+Maybe limited?
+Windows to other dimensions?
+
+*/
+
+//Power//
+/*
+Jadepower -(her manually pushing something)
+Steam turbine
+Nuclear
+Solar
+Geothermal
+Tidepower
+*/
+
+//Robotics
+/*
+Robot workshop
+Robot workstations
+Specific robots
+
+*/
+
+//Space//
+/*
+Rocketry
+*/
+
+//Stamina//
+/*
+Tier 0: Nothing
+T1: Sleeping bag & Tent => Camp, requires wood
+T2: Improvised hut, probably wood
+T3: Actual room, bed
+T4: luxury recuperating?
+T5 onwards ??
+
+*/
+
+//Storage - possible cap? Or less likely to lose stuff
+/*
+Perishable - Fridge
+Barn
+Underground bunker
+
+*/
+
+//Trade//
+/*
+Tradepost
+*/
+
+//Transport//
+/*
+Walking
+Bike (manual/motor)
+Raft/Surfboard
+Balloon/Glider
+Transportalisers
+Ship (to access second act)
+
+*/
+
+//Trash Recycler//
+/*
+Sorting manually
+Sorting station
+Smeltery
+Plastic recombinatortron
+
+*/
+
+//Water?//
+/*
+Base well - to start with
+Improvised rain catchers
+Aqueducts
+Desalination plant
+
+*/
+
+//Wood//
+/*
+Salvage House
+Chop Trees?
+Salvage sea?
+Tree farm (Timewarp or super fertiliser (radiation?/magic dog poop))
+
+*/
